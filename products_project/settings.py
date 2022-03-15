0f557dc7c06d8cc115d7c54ca87e0730e4c39aef
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-03-15 11:53:48 -0400

    changed database settings + created products app

--- products_project/settings.py
+++ products_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lnv3pgyfs!=yan2ig+hif+t$m4affe-xr72_j20bnypl0r!hfi'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +72,7 @@ WSGI_APPLICATION = 'products_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -121,3 +115,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from products_project.local_settings import *
+except ImportError: 
+    pass
